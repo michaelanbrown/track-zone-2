adf77a3577ffbc5601c51bf58c0be2ab9052d5fd
Michaela Brown <m_brownbrown@yahoo.com>
2023-05-04 16:46:24 -0700

    race year is 4 characters and greater than 1900

--- app/models/race.rb
+++ app/models/race.rb
@@ -3,7 +3,7 @@ class Race < ApplicationRecord
   belongs_to :length
 
   validates :name, presence: true
-  validates :year, presence: true, numericality: { greater_than: 0 }
+  validates :year, presence: true, numericality: { greater_than: 1900 }, length: { is: 4 }
   validates :duration, presence: true, format: {with: /\A(?!00:00:00)[0-5][0-9]:[0-5][0-9]:[0-5][0-9]\Z/}, on: :update
   validates :user_id, presence: true
   validates :length_id, presence: true
