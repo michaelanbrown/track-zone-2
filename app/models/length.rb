13d9013f687b757ea7cfbf2bce09f63ae3ac9234
Michaela Brown <m_brownbrown@yahoo.com>
2023-05-04 08:55:25 -0700

    added validations to all models

--- app/models/length.rb
+++ app/models/length.rb
@@ -1,4 +1,7 @@
 class Length < ApplicationRecord
     has_many :races
     has_many :users, through: :races
+
+    validates :distance, numericality: { greater_than: 0 }
+    validates :unit_of_measurement, presence: true, inclusion: { in: %w(km mi m) }
 end
--- app/models/race.rb
+++ app/models/race.rb
@@ -1,4 +1,9 @@
 class Race < ApplicationRecord
   belongs_to :user
   belongs_to :length
+
+  validates :name, presence: true
+  validates :year, presence: true, numericality: { greater_than: 0 }
+  validates :user_id, presence: true
+  validates :length_id, presence: true
 end
