656900a2055d068b7bb2f2439539a36b78c1755b
Michaela Brown <m_brownbrown@yahoo.com>
2023-05-04 08:35:46 -0700

    race create action

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,2 +1,13 @@
 class RacesController < ApplicationController
+
+    def create
+        race = Race.create!(race_params)
+        render json: race, status: :created
+    end
+
+    private
+    
+    def race_params
+        params.permit(:name, :year, :user_id, :length_id)
+    end 
 end
