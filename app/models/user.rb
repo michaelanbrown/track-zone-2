8c89cd8caf74998638f8cc458578388c3a7a1699
Michaela Brown <m_brownbrown@yahoo.com>
2023-05-04 08:53:55 -0700

    added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ApplicationRecord
     has_many :races
     has_many :lengths, through: :races
 
+    validates :name, presence: true
+    validates :age, numericality: { greater_than_or_equal_to: 13 }
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true
+
     has_secure_password
 end
