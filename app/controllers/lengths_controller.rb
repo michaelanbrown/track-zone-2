e7e84439c32875ed3ccaa1b83b0928837a007215
Michaela Brown <m_brownbrown@yahoo.com>
2023-05-04 08:36:31 -0700

    length create action

--- app/controllers/lengths_controller.rb
+++ app/controllers/lengths_controller.rb
@@ -1,3 +1,13 @@
 class LengthsController < ApplicationController
 
+    def create
+        length = Length.create!(length_params)
+        render json: length, status: :created
+    end
+
+    private
+    
+    def length_params
+        params.permit(:distance, :measurement)
+    end 
 end
