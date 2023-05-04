bc2057ea55b28bcf62dc6594bcee43df8247e3b3
Michaela Brown <m_brownbrown@yahoo.com>
2023-05-04 08:34:50 -0700

    user create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,15 @@
 class UsersController < ApplicationController
 
+    def create
+        user = User.create!(user_params)
+        # session[:user_id] = user.id
+        render json: user, status: :ok
+    end
+
+    private 
+
+    def user_params
+        params.permit(:name, :age, :photo, :username, :email, :password)
+    end 
+
 end
