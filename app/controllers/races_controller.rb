94d7a4242ffa4721466e4ef2ccb0c2426ed0b6bf
Michaela Brown <m_brownbrown@yahoo.com>
2023-05-04 09:25:42 -0700

    dry code with full crud for races find_race method

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,7 +1,12 @@
 class RacesController < ApplicationController
+    before_action :find_race, only: [:show, :update, :destroy]
 
     def index 
         render json: Race.all, status: :ok
+    end
+
+    def show
+        render json: @race, status: :ok
     end
 
     def create
@@ -9,9 +14,23 @@ class RacesController < ApplicationController
         render json: race, status: :created
     end
 
+    def update
+        @race.update!(race_params)
+        render json: runner, status: :accepted
+    end
+
+    def destroy
+        @race.destroy
+        head :no_content 
+    end 
+
     private
     
     def race_params
         params.permit(:name, :year, :user_id, :length_id)
-    end 
+    end
+
+    def find_race
+        @race = Race.find(params[:id])
+    end
 end
