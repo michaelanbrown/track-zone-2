91638288541c67cd300cce33f28737dc2216c2bf
Michaela Brown <m_brownbrown@yahoo.com>
2023-08-30 18:45:33 -0700

    search bar placeholder says search here and state was changed from object to string

--- client/src/components/Users.js
+++ client/src/components/Users.js
@@ -5,9 +5,13 @@ import UserCard from './UserCard';
 
 function User({ users }) {
     const { currentUser, setCurrentUser } = useContext(UserContext);
-    const [search, setSearch] = useState({
-        search: ''
-    })
+    const [search, setSearch] = useState('')
+
+    function handleChange(e) {
+        setSearch({
+            [e.target.name] : e.target.value
+        });
+    }
 
     const userRender = users.map (user => {
         return (
@@ -18,7 +22,7 @@ function User({ users }) {
         return (
             <div>
                 <form>
-                    Search: <input type='text' name='search' value={search} onChange={handleChange} />
+                    Search: <input type='text' name='search' placeholder='Search Here' value={search} onChange={handleChange} />
                 </form>
                 { users ? userRender : null }
             </div>
