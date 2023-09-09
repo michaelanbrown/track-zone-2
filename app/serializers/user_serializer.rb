d7b817574bad49ce719b627ea8e1d26e712af428
Michaela Brown <m_brownbrown@yahoo.com>
2023-09-09 14:59:12 -0700

    added likes column to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :age, :photo, :username, :email, :password_digest
+  attributes :id, :name, :age, :photo, :username, :email, :password_digest, :likes
 
   has_many :races
   has_many :lengths
