ca7ed87cad1d40c3f052622fcbd5ff169abb5927
Michaela Brown <m_brownbrown@yahoo.com>
2023-09-10 13:20:17 -0700

    removed user search from skippingauthentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    skip_before_action :authenticate_user, only: [:index, :create, :update, :user_search]
+    skip_before_action :authenticate_user, only: [:index, :create, :update]
 
     def index 
         render json: User.all, status: :ok
